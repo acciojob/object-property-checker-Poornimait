1a853da3e37f41ec3c0cf93f3c2f08e804e59d4a
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-04 18:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	var x=sampleObject.hasOwnProperty(key);
+	return x;
 }
 
 // Do not change the code below
